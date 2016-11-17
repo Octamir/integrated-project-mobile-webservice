0ae69a5d97b2d9a6633aceda2b25b5b64326f4ac
octamir <timcocx788@gmail.com>
2016-11-17 11:52:15 +0100

    Added some routes

--- main.js
+++ main.js
@@ -6,15 +6,39 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
-const robot = 'http://192.168.56.101:5000'
+const robot = 'http://192.168.56.101:5000/';
 
 const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Test'));
 
+app.get('/get-name', (req, res) => {
+    axios.get(robot + 'getName')
+        .then((data) => {
+            console.log(data.data);
+            res.send(data.data);
+        });
+});
+
+app.get('/get-type', (req, res) => {
+    axios.get(robot + 'getType')
+        .then((data) => {
+            console.log(data.data);
+            res.send(data.data);
+        });
+});
+
+app.get('/get-battery', (req, res) => {
+    axios.get(robot + 'getBatteryLevel')
+        .then((data) => {
+            console.log(data.data);
+            res.send(data.data);
+        });
+});
+
 app.get('/actions/stand-init', (req, res) => {
-    axios.get(robot + '/actions/StandInit')
+    axios.get(robot + 'actions/StandInit')
         .then((data) => {
             console.log('StandInit');
             res.send(data.data);
@@ -22,7 +46,7 @@ app.get('/actions/stand-init', (req, res) => {
 });
 
 app.get('/actions/sit-relax', (req, res) => {
-    axios.get(robot + '/actions/SitRelax')
+    axios.get(robot + 'actions/SitRelax')
         .then((data) => {
             console.log('SitRelax');
             res.send(data.data);
@@ -30,7 +54,7 @@ app.get('/actions/sit-relax', (req, res) => {
 });
 
 app.get('/actions/stand-zero', (req, res) => {
-    axios.get(robot + '/actions/StandZero')
+    axios.get(robot + 'actions/StandZero')
         .then((data) => {
             console.log('StandZero');
             res.send(data.data);
@@ -38,7 +62,7 @@ app.get('/actions/stand-zero', (req, res) => {
 });
 
 app.get('/actions/lying-belly', (req, res) => {
-    axios.get(robot + '/actions/LyingBelly')
+    axios.get(robot + 'actions/LyingBelly')
         .then((data) => {
             console.log('LyingBelly');
             res.send(data.data);
@@ -46,7 +70,7 @@ app.get('/actions/lying-belly', (req, res) => {
 });
 
 app.get('/actions/lying-back', (req, res) => {
-    axios.get(robot + '/actions/LyingBack')
+    axios.get(robot + 'actions/LyingBack')
         .then((data) => {
             console.log('LyingBack');
             res.send(data.data);
@@ -54,7 +78,7 @@ app.get('/actions/lying-back', (req, res) => {
 });
 
 app.get('/actions/stand', (req, res) => {
-    axios.get(robot + '/actions/Stand')
+    axios.get(robot + 'actions/Stand')
         .then((data) => {
             console.log('Stand');
             res.send(data.data);
@@ -62,7 +86,7 @@ app.get('/actions/stand', (req, res) => {
 });
 
 app.get('/actions/crouch', (req, res) => {
-    axios.get(robot + '/actions/Crouch')
+    axios.get(robot + 'actions/Crouch')
         .then((data) => {
             console.log('Crouch');
             res.send(data.data);
@@ -70,7 +94,7 @@ app.get('/actions/crouch', (req, res) => {
 });
 
 app.get('/actions/sit', (req, res) => {
-    axios.get(robot + '/actions/Sit')
+    axios.get(robot + 'actions/Sit')
         .then((data) => {
             console.log('Sit');
             res.send(data.data);
@@ -78,5 +102,3 @@ app.get('/actions/sit', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-["StandInit","SitRelax","StandZero","LyingBelly","LyingBack","Stand","Crouch","Sit"]
