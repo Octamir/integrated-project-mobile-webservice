4d52929720e9fafa44da2de5dc1aee4aadd05114
michaelvanderborght <michael.vanderborght.mv@gmail.com>
2016-11-17 11:43:00 +0100

    Added basic actions

--- main.js
+++ main.js
@@ -6,9 +6,77 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 
+const robot = 'http://192.168.56.101:5000'
+
 const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => res.send('Test'));
 
+app.get('/actions/stand-init', (req, res) => {
+    axios.get(robot + '/actions/StandInit')
+        .then((data) => {
+            console.log('StandInit');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/sit-relax', (req, res) => {
+    axios.get(robot + '/actions/SitRelax')
+        .then((data) => {
+            console.log('SitRelax');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/stand-zero', (req, res) => {
+    axios.get(robot + '/actions/StandZero')
+        .then((data) => {
+            console.log('StandZero');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/lying-belly', (req, res) => {
+    axios.get(robot + '/actions/LyingBelly')
+        .then((data) => {
+            console.log('LyingBelly');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/lying-back', (req, res) => {
+    axios.get(robot + '/actions/LyingBack')
+        .then((data) => {
+            console.log('LyingBack');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/stand', (req, res) => {
+    axios.get(robot + '/actions/Stand')
+        .then((data) => {
+            console.log('Stand');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/crouch', (req, res) => {
+    axios.get(robot + '/actions/Crouch')
+        .then((data) => {
+            console.log('Crouch');
+            res.send(data.data);
+        });
+});
+
+app.get('/actions/sit', (req, res) => {
+    axios.get(robot + '/actions/Sit')
+        .then((data) => {
+            console.log('Sit');
+            res.send(data.data);
+        });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+["StandInit","SitRelax","StandZero","LyingBelly","LyingBack","Stand","Crouch","Sit"]
