e8f689a41a4a92eab4e3a2dcac4e50495c388fa5
octamir <timcocx788@gmail.com>
2017-01-25 20:56:18 +0100

    Added support for dynamically retrieved actions

--- main.js
+++ main.js
@@ -46,75 +46,15 @@ app.get('/:ip/get-battery', (req, res) => {
         .catch((err) => errorHandler(res, err));
 });
 
-app.get('/:ip/actions/stand-init', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/StandInit`)
-        .then((data) => {
-            console.log('StandInit');
-            res.send(data.data);
-        })
+app.get('/:ip/get-actions', (req, res) => {
+    axios.get(`http://${req.params.ip}/getActions`)
+        .then((data) => res.send(data.data))
         .catch((err) => errorHandler(res, err));
 });
 
-app.get('/:ip/actions/sit-relax', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/SitRelax`)
-        .then((data) => {
-            console.log('SitRelax');
-            res.send(data.data);
-        })
-        .catch((err) => errorHandler(res, err));
-});
-
-app.get('/:ip/actions/stand-zero', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/StandZero`)
-        .then((data) => {
-            console.log('StandZero');
-            res.send(data.data);
-        })
-        .catch((err) => errorHandler(res, err));
-});
-
-app.get('/:ip/actions/lying-belly', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/LyingBelly`)
-        .then((data) => {
-            console.log('LyingBelly');
-            res.send(data.data);
-        })
-        .catch((err) => errorHandler(res, err));
-});
-
-app.get('/:ip/actions/lying-back', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/LyingBack`)
-        .then((data) => {
-            console.log('LyingBack');
-            res.send(data.data);
-        })
-        .catch((err) => errorHandler(res, err));
-});
-
-app.get('/:ip/actions/stand', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/Stand`)
-        .then((data) => {
-            console.log('Stand');
-            res.send(data.data);
-        })
-        .catch((err) => errorHandler(res, err));
-});
-
-app.get('/:ip/actions/crouch', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/Crouch`)
-        .then((data) => {
-            console.log('Crouch');
-            res.send(data.data);
-        })
-        .catch((err) => errorHandler(res, err));
-});
-
-app.get('/:ip/actions/sit', (req, res) => {
-    axios.get(`http://${req.params.ip}/actions/Sit`)
-        .then((data) => {
-            console.log('Sit');
-            res.send(data.data);
-        })
+app.get('/:ip/actions/:action', (req, res) => {
+    axios.get(`http://${req.params.ip}/actions/${req.params.action}`)
+        .then((data) => res.send(data.data))
         .catch((err) => errorHandler(res, err));
 });
 
--- robot-files/RAL.py
+++ robot-files/RAL.py
@@ -8,21 +8,21 @@ from random import randint
 import json
 from functools import update_wrapper
 from naoqi import ALProxy
- 
+
 app = Flask(__name__)
- 
+
 nao_host = "127.0.0.1"
 webserverIp = "0.0.0.0"
 nao_port = 9559
 battery = 100
 chargeStatus = True
-randNum = 0    
- 
+randNum = 0
+
 #logger = logger.Logger(4) # Initialize logger with level "debug"
- 
+
 #CROSSDOMAIN SHIZZLE
- 
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):  
+
+def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_to_all=True, automatic_options=True):
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
     if headers is not None and not isinstance(headers, basestring):
@@ -31,14 +31,14 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_t
         origin = ', '.join(origin)
     if isinstance(max_age, timedelta):
         max_age = max_age.total_seconds()
- 
+
     def get_methods():
         if methods is not None:
             return methods
- 
+
         options_resp = current_app.make_default_options_response()
         return options_resp.headers['allow']
- 
+
     def decorator(f):
         def wrapped_function(*args, **kwargs):
             if automatic_options and request.method == 'OPTIONS':
@@ -47,35 +47,35 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600, attach_t
                 resp = make_response(f(*args, **kwargs))
             if not attach_to_all and request.method != 'OPTIONS':
                 return resp
- 
+
             h = resp.headers
- 
+
             h['Access-Control-Allow-Origin'] = origin
             h['Access-Control-Allow-Methods'] = get_methods()
             h['Access-Control-Max-Age'] = str(max_age)
             if headers is not None:
                 h['Access-Control-Allow-Headers'] = headers
             return resp
- 
+
         f.provide_automatic_options = False
         return update_wrapper(wrapped_function, f)
     return decorator
- 
+
 #INDEX
 @app.route('/')
 @crossdomain(origin='*')
 def index():
     return "Hello Robotic World!"
- 
+
 #IP
 def getIP():
     return nao_host
- 
+
 @app.route('/getIP', methods=['GET'])
 @crossdomain(origin='*')
 def getIP_HTTP():
     return jsonify({'IP': getIP()}), 200
- 
+
 #TYPE
 def getType():
     """
@@ -95,7 +95,7 @@ def getType():
 @crossdomain(origin='*')
 def getType_HTTP():
     return jsonify({'type':getType()}), 200
- 
+
 #NAME
 def getName():
     """
@@ -110,68 +110,78 @@ def getName():
     """
 
     return 'Mister Nao'
- 
+
 @app.route('/getName', methods=['GET'])
 @crossdomain(origin='*')
 def getName_HTTP():
     return jsonify({'name':getName()}), 200
- 
+
 #CHARGE
 def charging():
     chargeStatus = True
     return "charging"
- 
+
 @app.route('/charge', methods=['GET'])
 @crossdomain(origin='*')
 def charging_HTTP():
     return jsonify({'chargeStatus':charging()}), 200
- 
+
 #UNPLUG
 def unplug():
     chargeStatus = False
     return "unplugged"
- 
+
 @app.route('/unplug', methods=['GET'])
 @crossdomain(origin='*')
 def unplug_HTTP():
     return jsonify({'chargeStatus':unplug()}), 200
- 
+
 #BATTERY LEVEL
 def getBatteryLevel():
     batt_json = json.dumps(battery)
     return batt_json
- 
+
 @app.route('/getBatteryLevel', methods=['GET'])
 @crossdomain(origin='*')
 def getBatteryLevel_HTTP():
     return jsonify({'batteryLevel':getBatteryLevel()}), 200
- 
+
 #GET ACTIONS
 def getActions():
-    return ["StandInit","SitRelax","StandZero","LyingBelly","LyingBack","Stand","Crouch","Sit"]
- 
+    return [
+            { 'route': 'actions/StandInit', 'displayName': 'Stand Init', 'type': 'standing' },
+            { 'route': 'actions/StandZero', 'displayName': 'Stand Zero', 'type': 'standing' },
+            { 'route': 'actions/Stand', 'displayName': 'Stand', 'type': 'standing' },
+            { 'route': 'actions/SitRelax', 'displayName': 'Sit Relax', 'type': 'sitting' },
+            { 'route': 'actions/Crouch', 'displayName': 'Crouch', 'type': 'sitting' },
+            { 'route': 'actions/Sit', 'displayName': 'Sit', 'type': 'sitting' },
+            { 'route': 'actions/LyingBelly', 'displayName': 'Lie on belly', 'type': 'lying' },
+            { 'route': 'actions/LyingBack', 'displayName': 'Lie on back', 'type': 'lying' },
+           ]
+
 @app.route('/getActions', methods=['GET'])
 @crossdomain(origin='*')
 def getActions_HTTP():
-    return jsonify({'actions':getActions()}), 200
- 
+    # Flask doesn't allow jsonify with lists
+    return json.dumps(getActions()), 200
+
 #DO ACTION
 def doAction(actionName):
     postureProxy = ALProxy("ALRobotPosture", nao_host, nao_port)
     postureProxy.goToPosture(str(actionName), 1.0)
     return postureProxy.getPostureFamily()
- 
+
 @app.route('/actions/<string:actionName>', methods=['GET'])
 @crossdomain(origin='*')
 def doAction_HTTP(actionName):
     return jsonify({'posture':doAction(actionName)}), 200
- 
+
 #ASK
 def ask(text):
     tts = ALProxy("ALTextToSpeech", nao_host, nao_port)
     tts.say(str(text))
     return text
- 
+
 @app.route('/ask/<string:text>', methods=['GET'])
 @crossdomain(origin='*')
 def ask_HTTP(text):
@@ -187,7 +197,7 @@ def move(x,y,d):
     theta = float(d)
     motionProxy.moveTo(xCoo, yCoo, theta)
     return [x,y,d]
- 
+
 @app.route('/move/<int:x>/<int:y>/<int:d>', methods=['GET'])
 @crossdomain(origin='*')
 def move_HTTP(x,y,d):
@@ -200,14 +210,14 @@ def openHand(hand):
     else:
         motionProxy.openHand("RHand")
     return hand
- 
+
 @app.route('/move/hand/open/<string:hand>', methods=['GET'])
 @crossdomain(origin='*')
 def openHand_HTTP(hand):
     if not hand in ["LHand", "RHand"]:
         return jsonify({'error': 'Incorrect value for "hand"'}), 418
     return jsonify({'opened': openHand(hand)}), 200
-    
+
 def closeHand(hand):
     motionProxy = ALProxy("ALMotion", nao_host, nao_port)
     if(hand == "LHand"):
@@ -215,18 +225,18 @@ def closeHand(hand):
     else:
         motionProxy.closeHand("RHand")
     return hand
- 
+
 @app.route('/move/hand/close/<string:hand>', methods=['GET'])
 @crossdomain(origin='*')
 def closeHand_HTTP(hand):
     if not hand in ["LHand", "RHand"]:
         return jsonify({'error': 'Incorrect value for "hand"'}), 418
     return jsonify({'closed': closeHand(hand)}), 200
- 
+
 #GET ALL FROM ROBOT
 def getRobot():
     return jsonify({'ip':getIP(), 'type':getType(), 'name':getName(), 'batteryLevel':getBatteryLevel(), 'chargeStatus':chargeStatus, 'posture':doAction("StandInit"), 'actions':getActions()})
- 
+
 @app.route('/getRobot', methods=['GET'])
 @crossdomain(origin='*')
 def getRobot_HTTP():
@@ -234,7 +244,7 @@ def getRobot_HTTP():
 
 def getPicture_HTTP():
     try:
-        photoCaptureProxy = ALProxy("ALPhotoCapture", nao_host, nao_port) 
+        photoCaptureProxy = ALProxy("ALPhotoCapture", nao_host, nao_port)
         photoCaptureProxy.setResolution(2)
         photoCaptureProxy.setPictureFormat("png")
         photoCaptureProxy.takePictures(1, "/home/nao/recordings/cameras/", "image")
